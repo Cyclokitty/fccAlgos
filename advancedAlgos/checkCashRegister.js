2b0e42d428e8d6192cc58257bd5526d155d3ee70
Laura Vingada <laura@vingada.com>
2017-06-28 18:35:45 -0400

    updated checkCashRegister algo

--- advancedAlgos/checkCashRegister.js
+++ advancedAlgos/checkCashRegister.js
@@ -1,32 +1,51 @@
 function checkCashRegister(price, cash, cid) {
-  Array.prototype.concatAll = function() {
-  var results = [];
-  this.forEach(function(subArray) {
-    // ------------ INSERT CODE HERE! ----------------------------
-    // Add all the items in each subArray to the results array.
-    if (Array.isArray(subArray)) {
-      results.push(...subArray);
-    }
-    // ------------ INSERT CODE HERE! ----------------------------
-  });
+  // this will hold the type of money and amounts
+  var changeBack = [];
 
-  return results;
-};
+  // this is the change to go back to customer
   var change = cash - price;
-  var total = cid.concatAll();
-  console.log(total);
-   // map total for the amount in cid
+  console.log("The change you should get: ", change);
 
-  if (change === cid ) {
+  // this is the total amount in the cash register
+  const total = cid.reduce((accum, amount) => {
+    return accum += amount[1];
+  }, 0);
+
+  console.log('total: ', total.toFixed(2));
+
+  if (change === total ) {
     return "Closed";
   }
 
-  if (change > cid) {
+  if (change > total) {
     return "Insufficient Funds";
   }
+
+  // coin and bills value:
+  let values = { 'ONE HUNDRED': 100, 'TWENTY': 20, 'TEN': 10, 'FIVE': 5, 'ONE': 1, 'QUARTER': 0.25, 'DIME': 0.10, 'NICKEL': 0.05, 'PENNY': 0.01 };
+
+  // let's solve this algo!
+  for (var item in values) {
+    while (change >= values[item]) {
+      changeBack.push([item, values[item]]);
+      change -= values[item];
+      for (var i = 0; i < cid.length; i++) {
+        if (cid[i][0] === item) {
+          cid[i][1] -= values[item];
+        }
+      }
+    }
+  }
+
   // Here is your change, ma'am.
-  return change;
+
+  // checking if cid changes after change made
+  console.log(cid);
+
+  return changeBack;
 }
+
+
 
 // Example cash-in-drawer array:
 // [["PENNY", 1.01],
@@ -39,4 +58,4 @@ function checkCashRegister(price, cash, cid) {
 // ["TWENTY", 60.00],
 // ["ONE HUNDRED", 100.00]]
 
-checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+checkCashRegister(21.50, 50.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
