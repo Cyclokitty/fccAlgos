a160c529edb83e5491b5439ba56572b357b4043c
Laura Vingada <laura@vingada.com>
2017-07-09 15:46:41 -0400

    almost completed checkCash algo

--- advancedAlgos/checkCashRegister.js
+++ advancedAlgos/checkCashRegister.js
@@ -4,14 +4,14 @@ function checkCashRegister(price, cash, cid) {
 
   // this is the change to go back to customer
   var change = cash - price;
-  console.log("The change you should get: ", change);
+  //console.log("The change you should get: ", change);
 
   // this is the total amount in the cash register
   const total = cid.reduce((accum, amount) => {
     return accum += amount[1];
   }, 0);
 
-  console.log('total: ', total.toFixed(2));
+//  console.log('total: ', total.toFixed(2));
 
   if (change === total ) {
     return "Closed";
@@ -37,12 +37,25 @@ function checkCashRegister(price, cash, cid) {
     }
   }
 
-  // Here is your change, ma'am.
-
-  // checking if cid changes after change made
   console.log(cid);
 
-  return changeBack;
+  // adding all the like change together:
+  let comparer = [].concat(...changeBack);
+  let categories =  comparer.filter(item => {
+    return isNaN(item);
+  });
+
+  var counts = {};
+  categories.forEach(item => {
+    counts[item] = (counts[item] || 0) + 1;
+  });
+
+  let yourChange = [];
+  for (var coinage in counts) {
+    yourChange.push([coinage, counts[coinage] * values[coinage]]);
+  }
+  // Here is your change, ma'am.
+  return yourChange;
 }
 
 
@@ -58,4 +71,8 @@ function checkCashRegister(price, cash, cid) {
 // ["TWENTY", 60.00],
 // ["ONE HUNDRED", 100.00]]
 
-checkCashRegister(21.50, 50.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+//checkCashRegister(21.50, 50.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+
+//checkCashRegister(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
+
+checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1.00], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
