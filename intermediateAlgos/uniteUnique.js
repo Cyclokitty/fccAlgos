d8fa91a1a8970dcca7b8b9ef544764ce7b44b61f
Laura Vingada <laura@vingada.com>
2017-07-08 21:18:08 -0400

    added alternative answer to uniteUnique algo

--- intermediateAlgos/uniteUnique.js
+++ intermediateAlgos/uniteUnique.js
@@ -11,3 +11,18 @@ function uniteUnique(arr) {
 }
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+// or:
+
+function flatStuff(arr) {
+  // take the arrays and turn into one array
+  let flat = [].concat(...arguments);
+  console.log(flat);
+
+  // filter thru the arrays and return the unique items
+  return flat.filter((item, index, self) => {
+    return self.indexOf(item) === index;
+  });
+}
+
+flatStuff([1, 3, 2], [5, 2, 1, 4], [2, 1]);
